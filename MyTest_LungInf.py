f8e1f3eaf5e138dfbb8e6fef4c6466764e92d3e5
rvignav <rvignav@gmail.com>
2020-08-24 21:53:05 -0700

    Update test script

--- MyTest_LungInf.py
+++ MyTest_LungInf.py
@@ -8,6 +8,7 @@ First Version: Created on 2020-05-13 (@author: Ge-Peng Ji)
 """
 
 import torch
+import imageio
 import torch.nn.functional as F
 import numpy as np
 import os
@@ -22,9 +23,9 @@ def inference():
     parser.add_argument('--testsize', type=int, default=352, help='testing size')
     parser.add_argument('--data_path', type=str, default='./Dataset/TestingSet/LungInfection-Test/',
                         help='Path to test data')
-    parser.add_argument('--pth_path', type=str, default='./Snapshots/save_weights/Semi-Inf-Net/Semi-Inf-Net-100.pth',
+    parser.add_argument('--pth_path', type=str, default='./Snapshots/save_weights/Inf-Net/Inf-Net-100.pth',
                         help='Path to weights file. If `semi-sup`, edit it to `Semi-Inf-Net/Semi-Inf-Net-100.pth`')
-    parser.add_argument('--save_path', type=str, default='./Results/Lung infection segmentation/Semi-Inf-Net/',
+    parser.add_argument('--save_path', type=str, default='./Results/Lung infection segmentation/Inf-Net/',
                         help='Path to save the predictions. if `semi-sup`, edit it to `Semi-Inf-Net`')
     opt = parser.parse_args()
 
@@ -36,8 +37,8 @@ def inference():
 
     model = Network()
     # model = torch.nn.DataParallel(model, device_ids=[0, 1]) # uncomment it if you have multiply GPUs.
-    model.load_state_dict(torch.load(opt.pth_path, map_location={'cuda:1':'cuda:0'}))
-    model.cuda()
+    model.load_state_dict(torch.load(opt.pth_path, map_location=torch.device('cpu')))
+    # model.cuda()
     model.eval()
 
     image_root = '{}/Imgs/'.format(opt.data_path)
@@ -48,7 +49,7 @@ def inference():
     for i in range(test_loader.size):
         image, name = test_loader.load_data()
 
-        image = image.cuda()
+        # image = image.cuda()
 
         lateral_map_5, lateral_map_4, lateral_map_3, lateral_map_2, lateral_edge = model(image)
 
@@ -56,7 +57,7 @@ def inference():
         # res = F.upsample(res, size=(ori_size[1],ori_size[0]), mode='bilinear', align_corners=False)
         res = res.sigmoid().data.cpu().numpy().squeeze()
         res = (res - res.min()) / (res.max() - res.min() + 1e-8)
-        misc.imsave(opt.save_path + name, res)
+        imageio.imwrite(opt.save_path + name, res)
 
     print('Test Done!')
 
